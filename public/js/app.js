47c01eef5e49dddef8a0a98208b330e828d7d956
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-07 08:52:02 -0300

    added map function to component ProductList to handle Product component

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,17 @@
 class ProductList extends React.Component {
   render() {
-    const product = Seed.products[0];
-    return (
-      <div className="ui unstackable items">
-        <Product
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          producImageUrl={product.producImageUrl}
-        />
-      </div>
-    );
+    const productComponents = Seed.products.map((product) => (
+      <Product
+        id={"product-" + product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
+    return <div className="ui unstackable items">{productComponents}</div>;
   }
 }
 

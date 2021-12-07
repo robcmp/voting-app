0eec2c47e9ae6e27cd3bf79cde0346da8563ed62
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-07 08:28:11 -0300

    added props to the product component

--- public/js/app.js
+++ public/js/app.js
@@ -22,19 +22,25 @@ class Product extends React.Component {
     return (
       <div className="item">
         <div className="image">
-          <img src="images/products/image-aqua.png"></img>
+          <img src={this.props.productImageUrl} />
         </div>
         <div className="middle aligned content">
-          <div className="description">
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actor, delivered in just two week's</p>
+          <div className="header">
+            <a>
+              <i className="large caret up icon" />
+            </a>
+            {this.props.votes}
           </div>
-          <div>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
+          </div>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
               className="ui avatar image"
-              src="images/avatars/daniel.jpg"
-            ></img>
+              src={this.props.submitterAvatarUrl}
+            />
           </div>
         </div>
       </div>

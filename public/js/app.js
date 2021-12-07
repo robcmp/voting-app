982266a23c1b00870db07e5640538ad0ba1eab6a
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-07 08:23:03 -0300

    added props to product component to be able to work with seed data

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,17 @@
 class ProductList extends React.Component {
   render() {
+    const product = Seed.products[0];
     return (
       <div className="ui unstackable items">
-        <Product />
+        <Product
+          id={product.id}
+          title={product.title}
+          description={product.description}
+          url={product.url}
+          votes={product.votes}
+          submitterAvatarUrl={product.submitterAvatarUrl}
+          producImageUrl={product.producImageUrl}
+        />
       </div>
     );
   }

e3621a32c7deee88e86b024f1bb71f02ac56c934
Roberto Mendoza <roberto.cmp16@gmail.com>
2021-12-09 08:22:53 -0300

    added to consructor handleProductUpVote bind, modified function handleProductUpVote to handle votes dynamically with .map and Object.assign, added to Product component id propertie and modified id for key

--- public/js/app.js
+++ public/js/app.js
@@ -4,18 +4,32 @@ class ProductList extends React.Component {
     this.state = {
       products: [],
     };
+
+    this.handleProductUpVote = this.handleProductUpVote.bind(this);
   }
   componentDidMount() {
     this.setState({ products: Seed.products });
   }
   handleProductUpVote(productId) {
-    console.log(productId + " was upvoted.");
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
   }
   render() {
     const products = this.state.products.sort((a, b) => b.votes - a.votes);
     const productComponents = products.map((product) => (
       <Product
-        id={"product-" + product.id}
+        key={"product-" + product.id}
+        id={product.id}
         title={product.title}
         description={product.description}
         url={product.url}
